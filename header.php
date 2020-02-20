4d054205e23b9fc279b16ce9b5bf8da27704cffa
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-20 08:49:43 -0700

    Added Feb 20 entry

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <html>
 <head>
 <title>code.dreamsense.com</title>
-<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" type="text/css" href="/style.css"/>
 </head>
 <body>
 
-<h1>code.dreamsense.com</h1>
+<h1><a href="/">code.dreamsense.com</a></h1>
 
 <h2><?php echo $title?></h2>
--- index.php
+++ index.php
@@ -1,36 +1,156 @@
 <?php
 
-$title = "February 19, 2020";
+$title = "February 20, 2020";
 
 ?>
 <?php include "header.php"?>
 
-<pre>
-public class HelloWorld {
+<p><a href="/2020/20200219.php">Yesterday</a> we talked about
+  the basic Hello World example, today we're talkinga bout dates.</p>
 
-  public staitc void main(String[] args) {
-    System.out.println("Hello World");
+<p>Every day has a date. It can be formatted in seveal different ways.</p>
+
+<p>Here are the ways Java can format dates:</p>
+
+<table border=0 cellspacing=3 cellpadding=0 summary="Chart shows pattern letters, date/time component, presentation, and examples.">
+    <caption>Found at: <a href="https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">Java DOCS</a></caption>
+     <tr bgcolor="#ccccff">
+         <th align=left>Letter
+         <th align=left>Date or Time Component
+         <th align=left>Presentation
+         <th align=left>Examples
+     <tr>
+         <td><code>G</code></td>
+         <td>Era designator</td>
+         <td>Text</td>
+         <td><code>AD</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>y</code></td>
+         <td>Year</td>
+         <td>Year</td>
+         <td><code>1996</code>; <code>96</code></td>
+     <tr>
+         <td><code>Y</code></td>
+         <td>Week year</td>
+         <td>Year</td>
+         <td><code>2009</code>; <code>09</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>M</code></td>
+         <td>Month in year</td>
+         <td>Month</td>
+         <td><code>July</code>; <code>Jul</code>; <code>07</code></td>
+     <tr>
+         <td><code>w</code></td>
+         <td>Week in year</td>
+         <td>Number</td>
+         <td><code>27</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>W</code></td>
+         <td>Week in month</td>
+         <td>Number</td>
+         <td><code>2</code></td>
+     <tr>
+         <td><code>D</code></td>
+         <td>Day in year</td>
+         <td>Number</td>
+         <td><code>189</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>d</code></td>
+         <td>Day in month</td>
+         <td>Number</td>
+         <td><code>10</code></td>
+     <tr>
+         <td><code>F</code></td>
+         <td>Day of week in month</td>
+         <td>Number</td>
+         <td><code>2</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>E</code></td>
+         <td>Day name in week</td>
+         <td>Text</td>
+         <td><code>Tuesday</code>; <code>Tue</code></td>
+     <tr>
+         <td><code>u</code></td>
+         <td>Day number of week (1 = Monday, ..., 7 = Sunday)</td>
+         <td>Number</td>
+         <td><code>1</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>a</code></td>
+         <td>Am/pm marker</td>
+         <td>Text</td>
+         <td><code>PM</code></td>
+     <tr>
+         <td><code>H</code></td>
+         <td>Hour in day (0-23)</td>
+         <td>Number</td>
+         <td><code>0</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>k</code></td>
+         <td>Hour in day (1-24)</td>
+         <td>Number</td>
+         <td><code>24</code></td>
+     <tr>
+         <td><code>K</code></td>
+         <td>Hour in am/pm (0-11)</td>
+         <td>Number</td>
+         <td><code>0</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>h</code></td>
+         <td>Hour in am/pm (1-12)</td>
+         <td>Number</td>
+         <td><code>12</code></td>
+     <tr>
+         <td><code>m</code></td>
+         <td>Minute in hour</td>
+         <td>Number</td>
+         <td><code>30</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>s</code></td>
+         <td>Second in minute</td>
+         <td>Number</td>
+         <td><code>55</code></td>
+     <tr>
+         <td><code>S</code></td>
+         <td>Millisecond</td>
+         <td>Number</td>
+         <td><code>978</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>z</code></td>
+         <td>Time zone</td>
+         <td>General time zone</td>
+         <td><code>Pacific Standard Time</code>; <code>PST</code>; <code>GMT-08:00</code></td>
+     <tr>
+         <td><code>Z</code></td>
+         <td>Time zone</td>
+         <td>RFC 822 time zone</td>
+         <td><code>-0800</code></td>
+     <tr bgcolor="#eeeeff">
+         <td><code>X</code></td>
+         <td>Time zone</td>
+         <td>ISO 8601 time zone</td>
+         <td><code>-08</code>; <code>-0800</code>;  <code>-08:00</code></td>
+ </table>
+
+<p>To use the date format, we can do the following:</p>
+
+<pre>
+  import java.util.Date;
+  import java.text.SimpleDateFormat;
+
+  public class TestDate {
+
+    public static void main(String[] args) {
+      Date d = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+      String output = sdf.format(d);
+      System.out.println(output);
+    }
   }
-}
 </pre>
 
-<p>Hello World is the most basic of programs. It's the first program people
-  are taught when they start programming. It's a simple program, one that just
-  outputs some text. The text doesn't have to be "Hello World" obviously. I've
-  also seen where a date was outputed instead of "Hello World". However you
-  wish to introduce someone to a programming language is up to you. But this is an
-  easy goto.</p>
+<p>This will output todays date in the <code>MM/dd/yyyy</code> format.</p>
 
-<p>I always wondered why a hello world was necessary and not a goodbye world. I
-  mean you could easily type it out saying goodbye instead of hello, right?</p>
-
-<pre>
-public class GoodbyeWorld {
-
-  public staitc void main(String[] args) {
-    System.out.println("Goodbye World");
-  }
-}
-</pre>
+<p>By putting any of the above characters in the <code>SimpleDateFormat</code>
+  constructor, you can output the date in any of the allowed formats.</p>
 
 <?php include "footer.php"?>
