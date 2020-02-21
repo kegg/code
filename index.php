3cde284c903b5376a09734c311da34f0151d77f9
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-21 13:02:23 -0700

    Added Java example

--- index.php
+++ index.php
@@ -32,6 +32,61 @@ $title = "February 21, 2020";
   say you wanted files sent in a specific format, you could use XML as that goto.
   grabbing data in this way, you can ensure that you get what you need.</p>
 
+<p>If we wanted to output the above xml, we could use Java and the
+  <code>XMLStreamWriter</code> class.</p>
+
+<pre>
+  import java.io.FileNotFoundException;
+  import java.io.FileOutputStream;
+  import java.util.HashMap;
+  import java.util.Map;
+
+  import javax.xml.stream.XMLOutputFactory;
+  import javax.xml.stream.XMLStreamException;
+  import javax.xml.stream.XMLStreamWriter;
+
+  public class WriteXml {
+
+    public static void main(String[] args) {
+      XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newInstance();
+      String filename = "data.xml";
+
+      try {
+        XMLStreamWriter xmlStreamWriter =
+          xmlOutputFactory.createXMLStreamWriter(
+          new FileOutputStream(filename), "UTF-8");
+
+          xmlStreamWriter.writeStartDocument("UTF-8", "1.0");
+          xmlStreamWriter.writeCharacters("\n");
+          xmlStreamWriter.writeStartElement("data");
+          xmlStreamWriter.writeCharacters("\n");
+
+          xmlStreamWriter.writeStartElement("firstName");
+          xmlStreamWriter.writeCharacters("John");
+          xmlStreamWriter.writeEndElement();
+          xmlStreamWriter.writeCharacters("\n");
+
+          xmlStreamWriter.writeStartElement("lastName");
+          xmlStreamWriter.writeCharacters("Smith");
+          xmlStreamWriter.writeEndElement();
+          xmlStreamWriter.writeCharacters("\n");
+
+          xmlStreamWriter.writeStartElement("phone");
+          xmlStreamWriter.writeCharacters("916-555-4418");
+          xmlStreamWriter.writeEndElement();
+          xmlStreamWriter.writeCharacters("\n");
+
+          xmlStreamWriter.writeEndDocument();
+
+          xmlStreamWriter.flush();
+          xmlStreamWriter.close();
+      } catch(XMLStreamException | FileNotFoundException e){
+        e.printStackTrace();
+      }
+    }
+  }
+</pre>
+
 <p>XML is slowly being replaced by JSON, but that's a story for another time.</p>
 
 <?php include "footer.php"?>
