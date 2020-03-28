0575ec991525e70db70abfd71ff426e43f3e4a54
Kyle Eggleston <kyle@dreamsense.com>
2020-03-28 12:05:59 -0600

    Added archive

---
+++ archive.php
@@ -0,0 +1,16 @@
+<?php
+
+$title = "Archive";
+
+?>
+<?php include "header.php"?>
+
+<p>
+<a href="/2020/20200219.php">Hello World</a><br/>
+<a href="/2020/20200220.php">Dates</a><br/>
+<a href="/2020/20200221.php">XML</a><br/>
+<a href="/2020/20200222.php">Reading XML</a><br/>
+<a href="/2020/20200305.php">Reading Text Files</a>
+</p>
+
+<?php include "footer.php"?>
--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <address>
 Kyle Eggleston<br/>
 <a href="mailto:kyle@dreamsense.com">kyle@dreamsense.com</a><br/>
-Copyright &copy; 2003 - 2020
+Copyright &copy; 2003 - 2020 <a href=/archive.php">archive</a>
 <p><a href="/PoweredByMacOSXLarge.gif"><img border="0" src="/PoweredByMacOSX.gif"/></p>
 </address>
 
